9b44abfafdc03c5c58ecf7ca71b7266f7a9de264
Christopher Valle <chrisbv@live.com>
2021-10-14 11:42:42 -0500

    commit tree 31f677c5149fb1d589cba75776d483393a832c10

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+// function findWin(element, index, array) {
+//     return (element === "W")
+// }
 
+// function superbowlWin(s){
+//     s.find( function(t) { return t === "W" });
+    
+// }
+
+superbowlWin = (s) => {
+    const result = s.find( s => s.result === "W");
+    return !!result ? result.year : undefined;
+}
